ffe825d41e5879e6059eda1d005f1184224b520f
HugoLeonardo-git <hugo05boss@gmail.com>
2022-06-02 20:09:19 -0300

    Add files via upload

---
+++ ProjetoBancoSolution/ProjetoBancoSolution/ProjetoBanco/Classes/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoBanco.Classes
+{
+    public class Cliente
+    {
+        private Cliente() { }
+        public string Nome { get; private set; }
+        public string CPF { get; private set; }
+        public string Telefone { get; private set; }
+        public string Endereço { get; private set; }
+        public DateTime DataNascimento { get; private set; }
+        public string IDConta { get; private set; }
+
+        public static Cliente CreateCliente(string nome, string cpf, string endereço, string telefone, DateTime datanascimento) =>
+        
+        new Cliente { Nome = nome, CPF = cpf, Telefone = telefone, Endereço = endereço, DataNascimento = datanascimento };
+        
+    }
+}
---
+++ ProjetoBancoSolution/ProjetoBancoSolution/ProjetoBanco/Classes/Investir.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoBanco.Classes
+{
+    
+    public class Investimento : Conta
+    {
+        public Investimento(Cliente cliente, DateTime numero, decimal saldo)
+            : base(cliente, numero, saldo)
+        { }
+
+        public override bool Depositar(decimal valorDeposito)
+        {
+            if (Saldo <= 0m)
+            {
+                MensagemTransacoes = $"O saldo é insuficiente para Investir. Sua conta está com o valor atual de {Saldo}";
+                return false;
+            }
+
+            if (Saldo < valorDeposito)
+            {
+                MensagemTransacoes = $"Valor solicitado para o investimento é {valorDeposito} e o Saldo é {Saldo}";
+                Saldo = 0;
+                return false;
+            }
+
+
+            Saldo = valorDeposito * 1.5m; //.m necessário pra contar como decimal
+            MensagemTransacoes = $"Deposito realizado com sucesso! Seu Investimento irá render R${Saldo} em 5 anos!";
+
+            return true;
+        }
+
+        public override bool Investir(decimal valorInvestir)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Sacar(decimal valorSaque)
+        {
+            if (Saldo <= 0m)
+            {
+                MensagemTransacoes = $"O saldo é insuficiente para saque. Sua conta está com o valor atual de {Saldo}";
+                return false;
+            }
+
+            if (Saldo < valorSaque)
+            {
+                MensagemTransacoes = $"Valor solicitado para o saque é {valorSaque} e o Saldo é {Saldo}";
+                return false;
+            }
+
+            Saldo -= valorSaque + 0.1m;
+            return true;
+        }
+    }
+}
+    
+
